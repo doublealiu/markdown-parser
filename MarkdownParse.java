ef7dd15f2c8b2cc506a88fb0eddee0e22b01b37a
doublealiu <46761322+doublealiu@users.noreply.github.com>
2022-04-14 11:22:26 -0700

    I think I got the right solution here.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,16 +7,32 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
+    // checks if there is something in front of
+    // pos that "looks like a link exists, we
+    // still don't know if one actually exists yet
+    public static boolean linkExists(String str, int pos) {
+        return str.indexOf("[", pos) != -1 && str.indexOf("]", pos) != -1 && str.indexOf("(", pos) != -1 && str.indexOf(")", pos) != -1;
+    }
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while(currentIndex < markdown.length() && linkExists(markdown, currentIndex)) {
+            System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
+            while (markdown.indexOf("[", openBracket + 1) != -1 && markdown.indexOf("[", openBracket + 1) < closeBracket) {
+                openBracket = markdown.indexOf("(", openBracket + 1);
+            }
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            while (markdown.indexOf("(", openParen + 1) != -1 && markdown.indexOf("(", openParen + 1) < closeParen) {
+               openParen = markdown.indexOf("(", openParen + 1);
+           }
+            if (closeBracket + 1 == openParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
 
@@ -27,6 +43,7 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
+        System.out.println(content.length());
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
     }
