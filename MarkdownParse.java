db5c95d4393dbe1408b2983e1cac1e93a4bc4c7c
doublealiu <46761322+doublealiu@users.noreply.github.com>
2022-04-21 10:57:51 -0700

    Added more test cases from main markdown-parser repo

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,5 +1,6 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,7 +20,7 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length() && linkExists(markdown, currentIndex)) {
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             while (markdown.indexOf("[", openBracket + 1) != -1 && markdown.indexOf("[", openBracket + 1) < closeBracket) {
@@ -41,7 +42,20 @@ public class MarkdownParse {
 
 
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
+        //testIndividual
+        testManyFileX();
+    }
+
+    public static void testManyFileX() throws IOException {
+        for (int i = 2; i <= 8; i++) {
+            System.out.println("test-file" + i + ": ");
+            testIndividual("test" + File.separator + "test-file" + i + ".md");
+            System.out.println();
+        }
+    }
+
+    public static void testIndividual(String f) throws IOException {
+        Path fileName = Path.of(f);
         String content = Files.readString(fileName);
         //System.out.println(content.length());
         ArrayList<String> links = getLinks(content);
